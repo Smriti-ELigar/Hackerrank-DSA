44237f1f354457be803111710dff22f631ff7afc
Smriti-ELigar <smritieligar1005@gmail.com>
2024-11-09 10:22:00 +0530

    Create 10.java

---
+++ Java-Level0/MOD2-CONDITIONAL/10.java
@@ -0,0 +1,22 @@
+//Write a program to print the respective month name based on given input
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String[] month ={"January", "February", "March", "April", "May","June","July","August","September","October","November","December"};
+        if(a<1 || a>12){
+            System.out.print("Invalid");
+        }
+        else{
+         System.out.print(month[a-1]);   
+         //System.out.print(month[a - 1]);       
+        }
+    }
+}

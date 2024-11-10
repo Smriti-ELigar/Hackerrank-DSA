86eaeed2cf842a46eb1e5fdb31ffd11761cfc8ad
Smriti Eligar <smritieligar1005@gmail.com>
2024-11-10 13:54:13 +0530

    Create 7.java

---
+++ Java-Level0/MOD3-Looping/7.java
@@ -0,0 +1,27 @@
+//program to find the LCM of the given two numbers.
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int num1 = sc.nextInt(); 
+        int num2 = sc.nextInt(); 
+        int lcm = findlcm(num1, num2); 
+        System.out.println(lcm);
+    }
+    static int findlcm(int a, int b){
+        return (a*b)/findgcd(a,b);
+    }
+    static int findgcd(int a,int b){
+        if(b==0)return a;
+        return findgcd(b,a%b);
+    }
+}
+//he Least Common Multiple (LCM) of two numbers is the smallest number that is a multiple of both.
+//findLCM method calculates the LCM using the formula 
+//findGCD method calculates the GCD using the Euclidean algorithm.
